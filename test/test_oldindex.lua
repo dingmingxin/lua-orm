c8f506f6753619e0a45a74f6a525c01a9de5f612
LiuYang <fpigparadise@gmail.com>
2015-12-08 15:35:40 +0800

    upgrade __oldindex test case

--- test/test_oldindex.lua
+++ test/test_oldindex.lua
@@ -1,11 +1,16 @@
 local mt = {}
+local trigger = false
 function mt.__newindex(obj, k, v)
-    print('__newindex', obj, k, v)
+    print('__newindex', obj, k, obj[k], v)
+    assert(obj[k] == nil)
+    trigger = true
     rawset(obj, k, v)    
 end
 
 function mt.__oldindex(obj, k, v)
-    print('__oldindex', obj, k, v)
+    print('__oldindex', obj, k, obj[k], v)
+    assert(obj[k] ~= nil)
+    trigger = true
     rawset(obj, k, v)
 end
 
@@ -17,14 +22,29 @@ local function create_obj()
 end
 
 local obj_a = create_obj()
-print('test0', obj_a.a)
+print('test new 1', obj_a.a)
+trigger = false
 obj_a.a = nil
-print('test1', obj_a.a)
+assert(trigger)
+
+print('test new 2')
+trigger = false
 obj_a.a = 1
-print('test2', obj_a.a)
+assert(trigger)
+
+print('test old 1')
+trigger = false
 obj_a.a = 2
-print('test3', obj_a.a)
+assert(trigger)
+
+print('test old 2')
+trigger = false
 obj_a.a = nil
-print('test4', obj_a.a)
+assert(trigger)
+
+print('test new 3')
+trigger = false
 obj_a.a = 1
-print('test5', obj_a.a)
+assert(trigger)
+
+print('test index', obj_a.a)
